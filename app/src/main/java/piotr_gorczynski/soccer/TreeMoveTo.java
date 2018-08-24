3f4221fda968a2977de8b10cc592edc92c4be6b7
piotr-gorczynski <piotr.gorczynski@gmail.com>
2018-08-24 09:14:10 +0200

    Logic for Android player added based on MINMAX algorithm

---
+++ app/src/main/java/piotr_gorczynski/soccer/TreeMoveTo.java
@@ -0,0 +1,17 @@
+package piotr_gorczynski.soccer;
+
+import java.util.ArrayList;
+
+public class TreeMoveTo {
+    public int X;
+    public int Y;
+    public int P;
+    boolean bounce;
+    ArrayList<TreeMoveTo> NextMoves = new ArrayList<TreeMoveTo>();
+
+    public TreeMoveTo(int x, int y, int p) {
+        X=x;
+        Y=y;
+        P=p;
+    }
+}
