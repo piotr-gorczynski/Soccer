21b8514edcc8edc0a8c03981e0cc8da278ba29c9
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-01-23 22:53:32 +0100

    Redirect logs to stdout for improved visibility in service check

--- gcp/cloud-functions/service-check/main.py
+++ gcp/cloud-functions/service-check/main.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request, Response
 from google.cloud import secretmanager
 import logging
+import sys
 
 app = Flask(__name__)
 
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s"
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    stream=sys.stdout,  # Direct logs to stdout
 )
 logger = logging.getLogger(__name__)
 
