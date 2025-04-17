40cd6274ecf33ec3c5334145da2e8c78386e4997
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-04-17 23:14:04 +0200

    Refactoring related to warnings

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/ExceptionHandler.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/ExceptionHandler.java
@@ -41,7 +41,7 @@ public class ExceptionHandler implements
         errorReport.append(LINE_SEPARATOR);
         errorReport.append("\n************ FIRMWARE ************\n");
         errorReport.append("SDK: ");
-        errorReport.append(Build.VERSION.SDK);
+        errorReport.append(Build.VERSION.SDK_INT);
         errorReport.append(LINE_SEPARATOR);
         errorReport.append("Release: ");
         errorReport.append(Build.VERSION.RELEASE);
