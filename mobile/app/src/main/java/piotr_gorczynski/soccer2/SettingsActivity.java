27c1288e16802ae61ba1e8e322b39d71bbc22262
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-04-19 23:39:44 +0200

    Design unified

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/SettingsActivity.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/SettingsActivity.java
@@ -2,22 +2,31 @@ package piotr_gorczynski.soccer2;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
+
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new LevelPreferenceFragment())
+                .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
     }
 
-    public static class LevelPreferenceFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.pref_android_level, rootKey);
-        }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
+
