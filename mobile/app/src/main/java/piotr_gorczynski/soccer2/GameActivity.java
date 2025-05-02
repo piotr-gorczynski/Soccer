efffcaae6236efe87a1c49308030f177c0c03fb7
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-05-02 23:38:10 +0200

    Starting match

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/GameActivity.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/GameActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -171,16 +170,6 @@ public class GameActivity extends AppCompatActivity {
 
             // 🔹 Show waiting screen immediately
             setContentView(R.layout.view_waiting_for_opponent);
-
-            // Enable full-screen immersive mode
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_IMMERSIVE
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            );
 
             db.collection("matches").document(matchId).get()
                     .addOnSuccessListener(doc -> {
