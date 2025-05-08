56ca69227c8e636fc54c87789049314a039bc235
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-05-08 22:31:34 +0200

    Real-time Move Sync

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/ExceptionHandler.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/ExceptionHandler.java
@@ -49,7 +49,7 @@ public class ExceptionHandler implements
                 LINE_SEPARATOR;
 
 
-        Log.d("Soccer", errorReport);
+        Log.d("Soccer2", errorReport);
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(10);
     }
