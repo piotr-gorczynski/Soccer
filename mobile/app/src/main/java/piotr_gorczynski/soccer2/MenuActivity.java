c9479a78a502171aa9035c05411768e0f9aebc97
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-06-06 23:11:02 +0200

    Firestore cache cleared

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/MenuActivity.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/MenuActivity.java
@@ -52,6 +52,11 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        /*
+        FirebaseFirestore.getInstance()
+                .clearPersistence()            // <-- clean cash
+                .addOnSuccessListener(v ->
+                        Log.d("TAG_Soccer","Firestore cache cleared"));*/
 
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(task -> {
