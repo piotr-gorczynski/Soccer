0a7e8c0fc3e9003dc2f091901a233dc994c78af4
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-11-23 19:47:13 +0100

    Increase FRAME_DURATION_MS for player sprites to 2000ms for smoother animations

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/KickPlayerSprite.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/KickPlayerSprite.java
@@ -14,7 +14,7 @@ final class KickPlayerSprite {
 
     private static final String TAG = "TAG_Soccer";
 
-    static final long FRAME_DURATION_MS = 350L;
+    static final long FRAME_DURATION_MS = 2000L;
     static final int FRAME_COUNT = 16;
 
     private static final int FRAME_WIDTH = 128;
