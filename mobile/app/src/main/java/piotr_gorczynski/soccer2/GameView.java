d2b58a00b6a581def92b1ebcb3292a4084f9cc11
piotr Górczyński <piotr.gorczynski@gmail.com>
2025-11-27 21:15:43 +0100

    Reduce frame duration for player sprites and adjust run delay cycles

--- mobile/app/src/main/java/piotr_gorczynski/soccer2/GameView.java
+++ mobile/app/src/main/java/piotr_gorczynski/soccer2/GameView.java
@@ -59,7 +59,7 @@ public class GameView extends View {
         public void run() {
             if (shouldPulse()) {
                 invalidate();
-                pulseHandler.postDelayed(this, 2000); // set 16 to get  ~60 FPS
+                pulseHandler.postDelayed(this, 32); // set 16 to get  ~60 FPS
             } else {
                 pulseScheduled = false;
             }
