12cb5c87067fa8d64ffdebf0c2b11d2d0f4d1321
piotr Górczyński <piotr.gorczynski@gmail.com>
2026-08-31 13:43:04 +0200

    Fix Firebase Functions dependencies for FID deployment
    Refs #1280

--- firebase/functions/on-tournament-complete/index.js
+++ firebase/functions/on-tournament-complete/index.js
@@ -1,5 +1,5 @@
 // functions/on-tournament-complete/index.js
-const functions = require('firebase-functions');
+const functions = require('firebase-functions/v1');
 const admin     = require('firebase-admin');
 
 // Only initialize if not already initialized
