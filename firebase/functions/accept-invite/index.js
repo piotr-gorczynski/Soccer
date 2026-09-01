c22a2fa3ba2827bc9dffdb4d549b58273b6cc215
piotr Górczyński <piotr.gorczynski@gmail.com>
2026-09-01 20:36:42 +0200

    Migrate Firebase functions to Node.js 22
    - Update function runtimes, package engines, lockfiles, and CI
    - Fix the accept-invite v1 SDK import
    - Remove invalid seed codebases and the Node.js shadow file
    - Deploy and verify all Node functions on the dev Firebase project

--- firebase/functions/accept-invite/index.js
+++ firebase/functions/accept-invite/index.js
@@ -1,5 +1,5 @@
 // functions/accept-invite/index.js        (CommonJS)
-const functions = require('firebase-functions');
+const functions = require('firebase-functions/v1');
 const admin     = require('firebase-admin');
 admin.initializeApp();
 
